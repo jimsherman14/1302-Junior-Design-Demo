c226f89dc61e745ec37727575b2176cc16ebb65a
Matthew Brown <matthewbrown@gatech.edu>
2021-11-25 12:53:14 -0500

    Implement login design changes

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import {
-    Button,
     Image,
     StyleSheet,
-    Text,
     View
 } from 'react-native';
+import { Button, Title } from 'react-native-paper';
 import Auth0 from 'react-native-auth0';
 
 // var credentials = require('./auth0-configuration.js');
@@ -15,7 +14,7 @@ const auth0 = new Auth0({ domain: 'dev-1b2jde8p.us.auth0.com', clientId: 'dX7LyM
 const LoginScreen = () => {
     const [accessToken, setAccessToken] = React.useState(null);
 
-    _onLogin = () => {
+    const login = () => {
         auth0.webAuth
             .authorize({
                 scope: 'openid profile email'
@@ -24,21 +23,26 @@ const LoginScreen = () => {
             .catch(error => console.log(error));
     };
 
-    _onLogout = () => {
+    const logout = () => {
         auth0.webAuth
             .clearSession({})
-            .then(success => setAccessToken(null))
+            .then(() => setAccessToken(null))
             .catch(error => console.log(error));
     };
 
+    if (accessToken) {
+        console.log('Successfully logged in!')
+    }
+
     return (
-        <View style = { styles.container }>
-            <Text style = { styles.header }> Welcome to Simian </Text>
-            <Image source={require('./images/simian_logo.png')}/>
-            <Text>
-                You are{ accessToken ? ' ' : ' not ' }logged in</Text>
-                <Button onPress = { accessToken ? this._onLogout : this._onLogin }
-                title = { accessToken ? 'Log Out' : 'Log In \n \n Register' }/>
+        <View style={styles.container}>
+            <View style={styles.logo}>
+                <Image source={require('../../assets/simian_logo.png')}/>
+            </View>
+            <Title style={styles.header}>Welcome to Simian</Title>
+            <Button style={styles.button} color='white' mode='contained' onPress={accessToken ? logout : login}>
+                Join now
+            </Button>
         </View >
     );
 }
@@ -48,12 +52,26 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#2BEBF5'
+        backgroundColor: '#FF9737'
+    },
+    logo: {
+        width: 225,
+        height: 225,
+        borderRadius: 112.5,
+        backgroundColor: 'white',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    button: {
+        width: '90%',
+        borderRadius: 15,
+        marginTop: 20
     },
     header: {
         fontSize: 30,
         textAlign: 'center',
-        margin: 10,
+        color: 'white',
+        marginTop: 15,
         fontWeight: 'bold'
     }
 });
